84ae9111d481b349358feceeb701b0fee7839438
Jack D <Jackdefroand@gmail.com>
2021-07-04 21:51:31 +0100

    syntax fix

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./SearchBar.css";
 
 const sortByOptions = {
-    BestMatch: "";
-    HighestRated: "";
+    BestMatch: "",
+    HighestRated: "",
     MostReviewed: ""
  }
